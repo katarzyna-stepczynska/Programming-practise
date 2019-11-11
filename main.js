809fcb7e867d66eab07ea92a49bca8d9d0646819
katarzyna-stepczynska <stepczynska.katarzyna@gmail.com>
2019-11-11 15:17:49 +0100

    Make submit's button

---
+++ main.js
@@ -0,0 +1,11 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+form.addEventListener('submit', function() {
+
+});
+
+const addItem = (e) => {
+    e.preventDefault();
+    console.log();
+}
