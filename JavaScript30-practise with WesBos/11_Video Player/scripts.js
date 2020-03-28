91939ce1d384554ca6d83bd11525b7ecfbb52dcb
Katarzyna <stepczynska.katarzyna@gmail.com>
2020-03-28 13:16:06 +0100

    Add project which learn about Key sequence detection with JavaScript

--- JavaScript30-practise with WesBos/11_Video Player/scripts.js
+++ JavaScript30-practise with WesBos/11_Video Player/scripts.js
@@ -10,10 +10,18 @@ const ranges = player.querySelectorAll('.player__slider');
 
 // functions
 const togglePlay = () => {
-    // const method = video.paused ? 'play' : 'pause';
-    // video[method]();
-    video.paused ? video.play() : video.pause();
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+    // video.paused ? video.play() : video.pause();
 };
-
+const updateBtn = () => {
+    const icon = this.paused ? '►' : '❚ ❚'; //here is a problem, in console icon play is not log, and 
+    console.log('Update the btn');
+    console.log(icon);
+    toggle.textContent = icon;
+};
 // hook listeners 
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateBtn);
+video.addEventListener('pause', updateBtn);
+toggle.addEventListener('click', togglePlay);
