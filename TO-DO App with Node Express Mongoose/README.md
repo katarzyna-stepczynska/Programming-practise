465d7a14bc4c2acca94acaf6321f2c0b2671b271
KStepczyńska <stepczynska.katarzyna@gmail.com>
2023-08-14 19:53:00 +0200

    cleaning

--- TO-DO App with Node Express Mongoose/README.md
+++
@@ -1,8 +0,0 @@
-# TO-DO Application with Node Express and Mongoose
-## Practise coding with Douglas Hirsch
-
-###### Introduction
-Create a todo application using node JS, Mongoose, and Express.
-
-###### Sources
-Douglas Hirsch: [link](https://www.youtube.com/watch?v=HakOBAbIfug&t=225s)
--- TO-DO App with Node Express Mongoose/models/todo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema; //capitalized is extremely important
-
-let todoSchema = new Schema({
-    description: String,
-    done: Boolean
-});
-//use the model on Mongoose
-module.exports = mongoose.model('Todo', todoSchema);
