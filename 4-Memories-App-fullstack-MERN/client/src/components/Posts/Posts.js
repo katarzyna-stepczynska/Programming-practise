1a7cc1bcee500848cff3780e88407511cd503bd9
KStepczyńska <stepczynska.katarzyna@gmail.com>
2023-08-18 20:49:44 +0200

    Add proxy to package.json, add getPosts action, correct trouble with cors

--- 4-Memories-App-fullstack-MERN/client/src/components/Posts/Posts.js
+++ 4-Memories-App-fullstack-MERN/client/src/components/Posts/Posts.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Post from "./Post/Post";
 import styles from "./Posts.module.css";
 
 const Posts = () => {
+  const posts = useSelector((state) => state.posts);
+  console.log(posts);
   return (
     <>
       <h1>POSTS</h1>
