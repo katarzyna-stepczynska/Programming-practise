b043d760d438590a92f6b6ecd75a39903f3152c9
KStepczyńska <stepczynska.katarzyna@gmail.com>
2023-08-29 21:21:30 +0200

    Work with form of Sign Up and Sign In

--- 4-Memories-App-fullstack-MERN/client/src/components/Auth/Auth.js
+++ 4-Memories-App-fullstack-MERN/client/src/components/Auth/Auth.js
@@ -8,6 +8,7 @@ import {
   Container,
 } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import LockIcon from "@mui/icons-material/Lock";
 import Input from "./Input";
 import classes from "./Auth.module.css";
 import "@fontsource/mulish";
@@ -28,23 +29,54 @@ const theme = createTheme({
 
 const Auth = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const isSignUp = false;
+  const [isSignUp, setIsSignUp] = useState(false);
 
   const handleSubmit = () => {};
 
   const handleChange = () => {};
 
-  const handleShowPassword = () =>
+  const switchMode = () => {
+    setIsSignUp((prevIsSignUp) => !prevIsSignUp);
+    handleShowPassword(false);
+  };
+
+  const handleShowPassword = () => {
     setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
-        <Paper sx={{ mt: 8, p: 2 }} elevation={3}>
-          <Avatar sx={{ m: 1, bgcolor: "color.dark" }}></Avatar>
-          <Typography variant="h5">
-            {isSignUp ? "Sign Up" : "Sign In"}
-          </Typography>
+        <Paper
+          sx={{
+            mt: 8,
+            p: 2,
+            borderRadius: "20px",
+            bgcolor: "rgba(255, 255, 255, 0.7)",
+          }}
+          elevation={3}
+        >
+          <Grid
+            xs
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Avatar sx={{ m: 1, bgcolor: "color.dark" }}>
+              <LockIcon />
+            </Avatar>
+            <Typography
+              sx={{
+                pb: 2,
+                fontFamily: "Mulish",
+              }}
+              variant="h5"
+              color="color.dark"
+            >
+              {isSignUp ? "Sign Up" : "Sign In"}
+            </Typography>
+          </Grid>
           <form sx={{ mt: 3, width: "100%" }} onSubmit={handleSubmit}>
             <Grid container spacing={2}>
               {isSignUp && (
@@ -87,7 +119,7 @@ const Auth = () => {
               )}
             </Grid>
             <Button
-              spacing={(3, 0, 2)}
+              sx={{ mt: 2 }}
               type="submit"
               fullWidth
               variant="contained"
@@ -95,6 +127,15 @@ const Auth = () => {
             >
               {isSignUp ? "Sign Up" : "Sign In"}
             </Button>
+            <Grid container justifyContent="center">
+              <Grid item>
+                <Button onClick={switchMode} sx={{ textTransform: "inherit" }}>
+                  {isSignUp
+                    ? "Already have an account? Sign In"
+                    : "Don't have an account? Sign Up"}
+                </Button>
+              </Grid>
+            </Grid>
           </form>
         </Paper>
       </Container>
--- 4-Memories-App-fullstack-MERN/client/src/components/Navbar/Navbar.js
+++ 4-Memories-App-fullstack-MERN/client/src/components/Navbar/Navbar.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { AppBar, Typography, Avatar, Toolbar, Button } from "@mui/material";
+import {
+  AppBar,
+  Typography,
+  Avatar,
+  Toolbar,
+  Button,
+  Grid,
+} from "@mui/material";
 import memoriesLogo from "../../images/logo.png";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
@@ -34,67 +41,59 @@ const Navbar = () => {
           bgcolor: "rgba(255, 255, 255, 0.7)",
         }}
         position="static"
-        color="inherit"
-        display="flex"
-        direction="row"
-        flexWrap="wrap"
       >
-        <div>
-          <Typography
-            sx={{
-              fontWeight: "bold",
-              fontFamily: "Mulish",
-              color: "#2e47ad",
-              textTransform: "uppercase",
-              textDecoration: "none",
-              mx: 2,
-            }}
-            variant="h5"
-            align="center"
-            component={Link}
-            to="/"
-          >
-            Happy Memories
-          </Typography>
-          <img
-            className={styles.image}
-            src={memoriesLogo}
-            alt="memories-icon"
-            height="30"
-          />
-        </div>
-        <Toolbar className={styles.toolbar}>
-          {user ? (
-            <div className={styles.profile}>
-              <Avatar
-                className={styles.avatar}
-                alt={user.result.name}
-                src={user.result.imageUrl}
-              >
-                {user.result.name.charAt(0)}
-              </Avatar>
-              <Typography className={styles.userName} variant="h6">
-                {user.result.name}
-              </Typography>
-              <Button
-                variant="contained"
-                className={styles.logout}
-                color="secondary"
-              >
-                Logout
-              </Button>
-            </div>
-          ) : (
-            <Button
-              sx={{ bgcolor: "button.main" }}
+        <Grid display="flex" justifyContent="space-between">
+          <Toolbar>
+            <Typography
+              sx={{
+                fontWeight: "bold",
+                fontFamily: "Mulish",
+                color: "#2e47ad",
+                textTransform: "uppercase",
+                textDecoration: "none",
+                mx: 2,
+              }}
+              variant="h5"
               component={Link}
-              to="/auth"
-              variant="contained"
+              to="/"
             >
-              Sign In
-            </Button>
-          )}
-        </Toolbar>
+              Happy Memories
+            </Typography>
+            <img src={memoriesLogo} alt="memories-icon" height="30" />
+          </Toolbar>
+          <Toolbar>
+            {user ? (
+              <div className={styles.profile}>
+                <Avatar
+                  className={styles.avatar}
+                  alt={user.result.name}
+                  src={user.result.imageUrl}
+                >
+                  {user.result.name.charAt(0)}
+                </Avatar>
+                <Typography className={styles.userName} variant="h6">
+                  {user.result.name}
+                </Typography>
+                <Button
+                  variant="contained"
+                  className={styles.logout}
+                  color="secondary"
+                >
+                  Logout
+                </Button>
+              </div>
+            ) : (
+              <Button
+                sx={{ bgcolor: "button.main" }}
+                component={Link}
+                to="/auth"
+                variant="contained"
+              >
+                Sign In
+              </Button>
+            )}
+          </Toolbar>
+        </Grid>
       </AppBar>
     </ThemeProvider>
   );
