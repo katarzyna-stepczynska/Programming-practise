b644f120e3a11c131ee993eaf7c4c1ea8a1beb14
KStepczyńska <stepczynska.katarzyna@gmail.com>
2023-09-24 20:56:41 +0200

    work with view of pagination add and search

--- 4-Memories-App-fullstack-MERN/client/src/components/Home/Home.js
+++ 4-Memories-App-fullstack-MERN/client/src/components/Home/Home.js
@@ -1,21 +1,40 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid, Paper } from "@mui/material";
+import {
+  Container,
+  Grow,
+  Grid,
+  Paper,
+  AppBar,
+  TextField,
+  Button,
+  Chip,
+} from "@mui/material";
 import { useDispatch } from "react-redux";
+import { useNavigate, useLocation } from "react-router-dom";
 import { getPosts } from "../../actions/posts";
 import Pagination from "../Pagination";
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
 
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 const Home = () => {
   const [currentId, setCurrentId] = useState(null);
   const dispatch = useDispatch();
+  const query = useQuery();
+  const navigate = useNavigate();
+  const page = query.get("page") || 1;
+  const searchQuery = query.get("searchQuery");
+
   useEffect(() => {
     dispatch(getPosts());
   }, [currentId, dispatch]);
 
   return (
     <Grow in>
-      <Container>
+      <Container maxWidth="xl">
         <Grid
           container
           justifyContent="space-between"
